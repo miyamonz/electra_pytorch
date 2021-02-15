7ca9d8bacbe96a2207bd2039a7032a0265595638
Koichi Miyamoto <m5813k@gmail.com>
2021-02-15 18:38:49 +0900

    fix mysorteddl

--- pretrain/_utils/mysorteddl.py
+++ pretrain/_utils/mysorteddl.py
@@ -22,7 +22,9 @@ class MySortedDL(TfmdDL):
 
         # Save attributes
         super().__init__(dataset, **kwargs)
-        store_attr('pad_idxs,srtkey_fc,cache_file', self)
+        self.pad_idxs = pad_idxs
+        self.srtkey_fc = srtkey_fc
+        self.cache_file = cache_file
 
     def create_item(self, i):
         return self.dataset[i]
@@ -30,11 +32,13 @@ class MySortedDL(TfmdDL):
     def create_batch(self, samples):
         # if self.pad_idx is False: return super().create_batch(samples)
         pad_idxs = self.pad_idxs
-        return tuple( pad_sequence(attr, batch_first=True, padding_value=pad_idxs[i]) if attr[0].shape else torch.stack(attr) for i, attr in enumerate(zip(*samples)))
+        return tuple(
+            pad_sequence(attr, batch_first=True, padding_value=pad_idxs[i]) if attr[0].shape else torch.stack(attr) 
+            for i, attr in enumerate(zip(*samples)))
 
-    @delegates(TfmdDL.new)
-    def new(self, dataset=None, **kwargs):
-        if 'get_idxs' in kwargs: # when Learner.get_preds, dataload has `get_idxs` will be cloned. So we need to prevent sorting again
-          kwargs['cache_file'] = self.cache_file
-        # We don't use filter_fc here cuz we can't don't validate certaion samples in dev/test set. 
-        return super().new(dataset=dataset, pad_idx=self.pad_idx, srtkey_fc=self.srtkey_fc, filter_fc=False, **kwargs)
+#     @delegates(TfmdDL.new)
+#     def new(self, dataset=None, **kwargs):
+#         if 'get_idxs' in kwargs: # when Learner.get_preds, dataload has `get_idxs` will be cloned. So we need to prevent sorting again
+#           kwargs['cache_file'] = self.cache_file
+#         # We don't use filter_fc here cuz we can't don't validate certaion samples in dev/test set. 
+#         return super().new(dataset=dataset, pad_idx=self.pad_idx, srtkey_fc=self.srtkey_fc, filter_fc=False, **kwargs)
