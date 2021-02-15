f692d4513c80e6c19a5e6a27e1ea53100d223ec6
Koichi Miyamoto <m5813k@gmail.com>
2021-02-15 17:19:18 +0900

    remove lines

--- pretrain/_utils/hf_dataset.py
+++ pretrain/_utils/hf_dataset.py
@@ -5,16 +5,21 @@ class HF_Dataset():
   def __init__(self, hf_dset, cols=None, hf_toker=None, neat_show=False, n_inp=1):
     print('HF_Dataset')
     # some default setting for tensor type used in decoding
-    if cols is None: cols = hf_dset.column_names
-    if isinstance(cols, list): 
-      if n_inp==1: 
-        if len(cols)==1: cols = {cols[0]: TensorText}
-        elif len(cols)==2: cols = {cols[0]: TensorText, cols[1]: TensorCategory}
-      else: cols = { c: noop for c in cols }
+    print('cols', cols)
+    print('neat_show', neat_show)
+    print('n_inp', n_inp)
+
+#     if cols is None: cols = hf_dset.column_names
+#     if isinstance(cols, list): 
+#       if n_inp==1: 
+#         if len(cols)==1: cols = {cols[0]: TensorText}
+#         elif len(cols)==2: cols = {cols[0]: TensorText, cols[1]: TensorCategory}
+#       else: cols = { c: noop for c in cols }
+
     assert isinstance(cols, dict)
     
     # make dataset output pytorch tensor
-    hf_dset.set_format( type='torch', columns=list(cols.keys()) )
+    hf_dset.set_format( type='torch', columns=list(cols.keys()))
 
     # store attributes
     self.pad_idx = hf_toker.pad_token_id
--- pretrain/_utils/mysorteddl.py
+++ pretrain/_utils/mysorteddl.py
@@ -1,85 +1,32 @@
-from functools import partial
-import json
-from tqdm import tqdm
 from torch.nn.utils.rnn import pad_sequence
 from fastai.text.all import *
 
-@delegates()
 class MySortedDL(TfmdDL):
-
     def __init__(self, dataset, srtkey_fc=None, filter_fc=False, pad_idx=None, cache_file=None, **kwargs):
         print('MySortedDl')
         # Defaults
-        if srtkey_fc is not False: srtkey_fc = lambda *x: len(x[0])
+        print('srtkey_fc', srtkey_fc)
+        print('filter_fc', filter_fc)
+        print('pad_idx', pad_idx)
+        print('cache_file', cache_file) # is only used when srtkey or filter fc exists
+        print('kwargs', kwargs)
+
         if pad_idx is None: pad_idx = getattr(dataset, 'pad_idx', False)
-        if isinstance(pad_idx, int): pad_idxs = [pad_idx] * len(dataset[0])
-        elif isinstance(pad_idx, (list, tuple)): pad_idxs = pad_idx
-        cache_file = Path(cache_file) if cache_file else None
-        idmap = list(range(len(dataset)))
+        print('pad_idx', pad_idx)  # 0
+        pad_idxs = [pad_idx] * len(dataset[0])
+        print('pad_idxs', pad_idxs)
+
 
         # Save attributes
         super().__init__(dataset, **kwargs)
-        store_attr('pad_idxs,srtkey_fc,filter_fc,cache_file,idmap', self)
+        store_attr('pad_idxs,srtkey_fc,cache_file', self)
 
-        # Prepare records for sorting / filtered samples
-        if srtkey_fc or filter_fc:
-          if cache_file and cache_file.exists():
-            # load cache and check
-            with cache_file.open(mode='r') as f: cache = json.load(f)
-            idmap, srtkeys = cache['idmap'], cache['srtkeys']
-            if srtkey_fc: 
-              assert srtkeys, "srtkey_fc is passed, but it seems you didn't sort samples when creating cache."
-              self.srtkeys = srtkeys
-            if filter_fc:
-              assert idmap, "filter_fc is passed, but it seems you didn't filter samples when creating cache."
-              self.idmap = idmap
-          else:
-            # overwrite idmap if filter, get sorting keys if sort
-            idmap = []; srtkeys = []
-            for i in tqdm(range_of(dataset), leave=False):
-                sample = self.do_item(i)
-                if filter_fc and not filter_fc(*sample): continue
-                if filter_fc: idmap.append(i)
-                if srtkey_fc: srtkeys.append(srtkey_fc(*sample))
-            if filter_fc: self.idmap = idmap
-            if srtkey_fc: self.srtkeys = srtkeys
-            # save to cache
-            if cache_file:
-              try: 
-                with cache_file.open(mode='w+') as f: json.dump({'idmap':idmap,'srtkeys':srtkeys}, f)
-              except: os.remove(str(cache_file))
-          # an info for sorting
-          if srtkey_fc: self.idx_max = np.argmax(self.srtkeys)
-          # update number of samples
-          if filter_fc: self.n = self.n = len(self.idmap)
-
-    def create_item(self, i): return self.dataset[self.idmap[i]]
+    def create_item(self, i): return self.dataset[i]
 
     def create_batch(self, samples):
-        if self.pad_idx is False: return super().create_batch(samples)
-        return tuple( pad_sequence(attr, batch_first=True, padding_value=self.pad_idxs[i]) if attr[0].shape and isinstance(self.pad_idxs[i], int) else torch.stack(attr) for i, attr in enumerate(zip(*samples)))
-
-    def get_idxs(self):
-        idxs = super().get_idxs()
-        if self.shuffle: return idxs
-        if self.srtkey_fc: return sorted(idxs, key=lambda i: self.srtkeys[i], reverse=True)
-        return idxs
-
-    def shuffle_fn(self,idxs):
-        if not self.srtkey_fc: return super().shuffle_fn(idxs)
-        idxs = np.random.permutation(self.n)
-        idx_max = np.where(idxs==self.idx_max)[0][0]
-        idxs[0],idxs[idx_max] = idxs[idx_max],idxs[0]
-        sz = self.bs*50
-        chunks = [idxs[i:i+sz] for i in range(0, len(idxs), sz)]
-        chunks = [sorted(s, key=lambda i: self.srtkeys[i], reverse=True) for s in chunks]
-        sort_idx = np.concatenate(chunks)
-
-        sz = self.bs
-        batches = [sort_idx[i:i+sz] for i in range(0, len(sort_idx), sz)]
-        sort_idx = np.concatenate(np.random.permutation(batches[1:-1])) if len(batches) > 2 else np.array([],dtype=np.int)
-        sort_idx = np.concatenate((batches[0], sort_idx) if len(batches)==1 else (batches[0], sort_idx, batches[-1]))
-        return iter(sort_idx)
+        # if self.pad_idx is False: return super().create_batch(samples)
+        pad_idxs = self.pad_idxs
+        return tuple( pad_sequence(attr, batch_first=True, padding_value=pad_idxs[i]) if attr[0].shape else torch.stack(attr) for i, attr in enumerate(zip(*samples)))
 
     @delegates(TfmdDL.new)
     def new(self, dataset=None, **kwargs):
