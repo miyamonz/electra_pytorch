28f3c8c9113d8b6d8b7ce8bed7bb979e41abab32
Koichi Miyamoto <m5813k@gmail.com>
2021-02-15 15:07:12 +0900

    remove deps for self

--- pretrain/download_datasets.py
+++ pretrain/download_datasets.py
@@ -5,7 +5,7 @@ from _utils.electra_dataprocessor import ELECTRADataProcessor
 from _utils.hugdatafast import HF_Datasets
 from _utils.hugdatafast import MySortedDL
 from pathlib import Path
-from fastai.text.all import FilteredBase, merge
+from fastai.text.all import FilteredBase, merge, DataLoaders
 from fastai.text.all import TensorText, noop
 
 
@@ -35,8 +35,9 @@ def download_and_dls(c, hf_tokenizer, num_proc=1):
             merged_dsets,
             cols={'input_ids': TensorText, 'sentA_length': noop},
             hf_toker=hf_tokenizer, n_inp=2)
+    hf_dset_dict = hf_dsets.hf_dsets
     dls = dataloaders(
-            hf_dsets,
+            hf_dset_dict,
             cache_dir='./datasets/electra_dataloader',
             cache_name='dl_{split}.json',
             bs=c.bs,
@@ -47,16 +48,14 @@ def download_and_dls(c, hf_tokenizer, num_proc=1):
     return dls
 
 
-def dataloaders(self, cache_dir, cache_name, device='cpu', dl_kwargs=None, **kwargs):
+def dataloaders(hf_dset_dict, cache_dir, cache_name, device='cpu', dl_kwargs=None, **kwargs):
     print('device', device)
     print('kwargs', kwargs) #{'bs': 128, 'num_workers': 3, 'pin_memory': False, 'shuffle_train': True, 'srtkey_fc': False}
-    hf_dsets = self.hf_dsets
-    print('self.hf_dsets', hf_dsets)
     print('dl_kwargs', dl_kwargs)
 
-    if dl_kwargs is None: dl_kwargs = [{} for _ in range(len(hf_dsets))]
+    if dl_kwargs is None: dl_kwargs = [{} for _ in range(len(hf_dset_dict))]
     elif isinstance(dl_kwargs, dict):
-        dl_kwargs = [ dl_kwargs[split] if split in dl_kwargs else {} for split in hf_dsets]
+        dl_kwargs = [ dl_kwargs[split] if split in dl_kwargs else {} for split in hf_dset_dict]
     # infer cache file names for each dataloader if needed
     dl_type = MySortedDL
     print('dl_type', dl_type)
@@ -65,7 +64,7 @@ def dataloaders(self, cache_dir, cache_name, device='cpu', dl_kwargs=None, **kwa
         cache_dir = Path(cache_dir)
         cache_dir.mkdir(exist_ok=True)
         if not cache_name.endswith('.json'): cache_name += '.json'
-        for i, split in enumerate(hf_dsets):
+        for i, split in enumerate(hf_dset_dict):
             filled_cache_name = dl_kwargs[i].pop('cache_name', cache_name.format(split=split))
             if 'cache_file' not in dl_kwargs[i]:
                 dl_kwargs[i]['cache_file'] = cache_dir/filled_cache_name
@@ -73,24 +72,23 @@ def dataloaders(self, cache_dir, cache_name, device='cpu', dl_kwargs=None, **kwa
     kwargs['drop_last'] = kwargs.pop('drop_last', False)
 
     # when corpus like glue/ax has only testset, set it to non-train setting
-    if list(hf_dsets.keys())[0].startswith('test'):
+    if list(hf_dset_dict.keys())[0].startswith('test'):
         kwargs['shuffle_train'] = False
         kwargs['drop_last'] = False
     
     print('kwargs', kwargs) #drop_last: false だけふえた
-    return FilteredBase_dataloaders(self, dl_kwargs=dl_kwargs, device=device, **kwargs)
+    return FilteredBase_dataloaders(hf_dset_dict, dl_kwargs=dl_kwargs, device=device, **kwargs)
 
 
-def FilteredBase_dataloaders(self, dl_kwargs, bs=64, val_bs=None, shuffle_train=True, n=None, path='.', device=None, **kwargs):
-    n_subsets = len(self.hf_dsets)  # HF_Dataset.n_subsets
+def FilteredBase_dataloaders(hf_dset_dict, dl_kwargs, bs=64, val_bs=None, shuffle_train=True, n=None, path='.', device=None, **kwargs):
+    n_subsets = len(hf_dset_dict)  # HF_Dataset.n_subsets
     print('n_subsets', n_subsets)  # = 1
     print('dl_kwargs', dl_kwargs)  # [{cache_file}]
     # if device is None: device=default_device()
     drop_last = kwargs.pop('drop_last', shuffle_train)
     print('drop_last', drop_last)
-    ds = list(self.hf_dsets.values())[0]
+    ds = list(hf_dset_dict.values())[0]
     dl = MySortedDL(ds, bs=bs, shuffle=shuffle_train, drop_last=drop_last, n=n, device=device,
         **merge(kwargs, dl_kwargs[0]))
     # dls = [dl] + [dl.new(self.subset(i), bs=(bs if val_bs is None else val_bs), shuffle=False, drop_last=False, n=None, **dl_kwargs[i]) for i in range(1, n_subsets)]
-    print('self._dbunch_type', self._dbunch_type)
-    return self._dbunch_type(dl, path=path, device=device)
+    return DataLoaders(dl, path=path, device=device)
