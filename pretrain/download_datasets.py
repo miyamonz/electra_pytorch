2c70e2238432b71706c68ce2c0cc4566d144b966
Koichi Miyamoto <m5813k@gmail.com>
2021-02-18 17:00:52 +0900

    make cache_dir option

--- pretrain/download_datasets.py
+++ pretrain/download_datasets.py
@@ -3,7 +3,7 @@ import datasets
 from _utils.electra_dataprocessor import ELECTRADataProcessor
 
 
-def download_dset(c, hf_tokenizer, num_proc):
+def download_dset(c, hf_tokenizer, cache_dir, num_proc):
     dsets = []
     ELECTRAProcessor = partial(
         ELECTRADataProcessor, hf_tokenizer=hf_tokenizer, max_length=c.max_length)
@@ -11,7 +11,7 @@ def download_dset(c, hf_tokenizer, num_proc):
     if 'wikipedia' in c.datas:
         print('load/download wiki dataset')
         wiki = datasets.load_dataset(
-            'wikipedia', '20200501.en', cache_dir='./datasets')['train']
+            'wikipedia', '20200501.en', cache_dir=cache_dir)['train']
         print('load/create data from wiki dataset for ELECTRA')
         e_wiki = ELECTRAProcessor(wiki).map(
             cache_file_name=f"1000_electra_wiki_{c.max_length}.arrow", num_proc=num_proc)
@@ -21,7 +21,7 @@ def download_dset(c, hf_tokenizer, num_proc):
     if 'openwebtext' in c.datas:
         print('load/download OpenWebText Corpus')
         owt = datasets.load_dataset(
-            'openwebtext', cache_dir='./datasets')['train']
+            'openwebtext', cache_dir=cache_dir)['train']
         print('load/create data from OpenWebText Corpus for ELECTRA')
         e_owt = ELECTRAProcessor(owt, apply_cleaning=False).map(
             cache_file_name=f"electra_owt_{c.max_length}.arrow", num_proc=num_proc)
